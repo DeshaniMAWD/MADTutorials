45839d0bb66fb8e34bbf57d0b5015351f6b6021f
DeshaniMAWD <it19015736@my.sliit.lk>
2020-08-08 22:16:27 +0530

    Tute2

---
+++ app/src/main/java/com/example/stringsactivity/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.stringsactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtvMsg2 = findViewById(R.id.tvMsg2);
+        txtvMsg2.setText(R.string.Msg2);
+
+        Log.i("Lifecycle", "OnCreate Called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("Lifecycle", "OnStart Called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("Lifecycle", "OnResume Called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("Lifecycle", "OnPause Called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i("Lifecycle", "OnStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i("Lifecyle", "OnDestroy Called");
+    }
+}
